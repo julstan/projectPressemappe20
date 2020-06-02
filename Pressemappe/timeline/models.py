1537953b4ce2e21f8e5aa436cec1558279a00c48
sarafischer <sf.fischer.fs@gmail.com>
2020-06-02 17:43:17 +0200

    Datenfelder in models.py umbenannt

--- Pressemappe/timeline/models.py
+++ Pressemappe/timeline/models.py
@@ -1,18 +1,21 @@
 from django.db import models
+
 class Person(models.Model):
+    pm20id = models.TextField ()
+    wikidata_object = models.TextField ()
+    name = models.TextField ()
+    birthday = models.DateTimeField ()
+    deathday = models.DateTimeField (null = True)
+    position_held = models.TextField ()
+    position_held_startdate = models.DateTimeField ()
+    position_held_enddate = models.DateTimeField ()
+    picture = models.TextField (null = True)
+        #enthält einen Link zu dem Bild
+    #eventuell hier später foreign key
+    country = models.TextField ()
 
-    pm20idLabdel =models.TextField()
-    person = models.TextField()
-    personLabel = models.TextField()
-    birthday = models.DateTimeField()
-    deathday = models.DateTimeField()
-    position_heldLabel= models.TextField()
-    startdate = models.DateTimeField()
-    picture = models.TextField()
-    #eventuell hier später forign key
-    country = models.TextField()
-
+#Überlegung: jedem Land die jeweiligen Personen zuordnen
 class Country (models.Model):
-    countryName = models.TextField()
+    name = models.TextField ()
+        #Name des Landes
 
-# Create your models here.
