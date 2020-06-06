7e7ff6e67915b8e16642ed516f265f04006f2323
Patricia Kaluza <petulacicle@gmail.com>
2020-06-06 20:04:50 +0200

    views.cpython-37.pyc, template.html und views.py aktualisiert

--- Pressemappe/timeline/views.py
+++ Pressemappe/timeline/views.py
@@ -1,7 +1,5 @@
-import csv
-
 from django.shortcuts import render, get_object_or_404
-from . import models
+from .models import Person
 # Create your views here.
 
 #Startseite wird aufgerufen unter http://localhost:8000/timeline/show#
@@ -11,5 +9,20 @@ def test(request):
 
 
 
+def person_detail_view(request):
+    obj = Person.object.get(id=1)
+    # context = {
+    #     'name' :  obj.name,
+    #     'birthday' : obj.birthday,
+    #     'deathday'  : obj.deathday,
+    #     'position_held' : obj.position_held,
+    #     'position_held_startdate' : obj.position_held_startdate,
+    #     'position_held_enddate' : obj.position_held_enddate,
+    #     'picture' : obj.picture,
+    # }
+    context = {
+        'object' :obj,
+    }
+    return render(request, "timeline/template.html", context)
 
 
