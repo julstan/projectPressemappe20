7ef5a2eb7f2af06266de41d790b3b0095f55c7f2
Patricia Kaluza <petulacicle@gmail.com>
2020-06-16 02:46:40 +0200

    Filteroptionen verschönert und erweitert um Regierungsbeginn/ende

--- Pressemappe/timeline/filters.py
+++ Pressemappe/timeline/filters.py
@@ -1,10 +1,15 @@
 #Filter basierend auf unserem Model
-
+from django.forms.widgets import TextInput
 import django_filters
+from django_filters import DateFilter, CharFilter
 from .models import *
+
+
 class PersonFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(label='Name')
-    country = django_filters.CharFilter(label='Land')
+    name = CharFilter(label='Name', lookup_expr='icontains',widget=TextInput(attrs={'placeholder': 'z.B. John F. Kennedy,...'}))
+    country = CharFilter(label='Land', lookup_expr='icontains',widget=TextInput(attrs={'placeholder': 'z.B. China'}))
+    StartDatum = DateFilter(label='Regierungsbeginn', field_name="position_held_startdate", lookup_expr='gte', widget=TextInput(attrs={'placeholder': 'z.B. 1789-04-30'}))  #größer und gleich als
+    EndDatum = DateFilter(label='Regierungsende', field_name="position_held_enddate", lookup_expr='lte',widget=TextInput(attrs={'placeholder': 'z.B. 2011-11-16'}))  #kleiner und gleich als
 
 
     class Meta:
