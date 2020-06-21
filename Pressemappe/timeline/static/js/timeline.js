67c69fbed3b7200bea5550228b10d8f282094b22
StefanAnS <stefan.strache@gmail.com>
2020-06-21 22:59:46 +0200

    Anpassung des Headers. Versuche den Header wieder Sticky zu bekommen leider nicht erfolgreich.

--- Pressemappe/timeline/static/js/timeline.js
+++ Pressemappe/timeline/static/js/timeline.js
@@ -14,3 +14,16 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 */
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
