0bea648aca885e64995f9105c058cbae59588094
IsabelleMeindl <im044@hdm-stuttgart.de>
2020-06-27 14:34:18 +0200

    Sprungmarker hinzugefügt

--- Pressemappe/timeline/static/js/timeline.js
+++ Pressemappe/timeline/static/js/timeline.js
@@ -16,11 +16,6 @@ for (i = 0; i < coll.length; i++) {
 */
 
 
-window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+
+
