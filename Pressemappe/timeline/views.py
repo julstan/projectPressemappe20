a1bd62a96955c07274d6452e577310f9cb98a202
sarafischer <sf.fischer.fs@gmail.com>
2020-07-05 13:58:12 +0200

    update-Funktion

--- Pressemappe/timeline/views.py
+++ Pressemappe/timeline/views.py
@@ -11,6 +11,7 @@ def timeline(request):
     personen = models.Person.objects.order_by('position_held_startdate')    #Alle Personendatensätze werden nach Startdatum der Position sortiert - Julika
     myFilter = PersonFilter(request.GET, queryset=personen)
     personen = myFilter.qs
+    update_database()
 
     for person in personen:
         person.jahr = person.position_held_startdate[:4]
@@ -48,34 +49,85 @@ def timeline(request):
     return render(request, "timeline/timeline_neu.html", context)
 
 
-def load_data ():
-    with open("../staatsoberhaupt_filter_test.csv") as csvdatei:
+def update_database ():
+
+    with open("../staatsoberhaupt_filter_test.csv", encoding="utf-8") as csvdatei:
         reader = csv.DictReader(csvdatei, delimiter=",")
+
+        count_new = 0
+        count_old = 0
+
+        new_data = True
+
         for row in reader:
-            person = Person ()
-            person.pm20id = row ["pm20id"]
-            person.wikidata_object = row ["wikidata_object"]
-            person.name = row ["name"]
-            person.birthday = row ["birthday"] [:10]
-            if row ["deathday"] != "":
-                person.deathday = row ["deathday"] [:10]
-            person.position_held = row ["position_held"]
-            person.position_held_startdate = row ["position_held_startdate"] [:10]
-            person.position_held_enddate = row ["position_held_enddate"] [:10]
-            if row ["picture"] != "":
-                person.picture = row ["picture"]
-            person.country = row ["country"]
-            person.gender = row ["gender"]
-            if row ["religionLabel"] != "":
-                person.religion = row ["religionLabel"]
-            if row ["predecessor"] != "":
-                person.predecessor = row ["predecessor"]
-            if row ["successor"] != "":
-                person.successor = row ["successor"]
-            person.save()
 
+            if row ["deathday"] == "":
+                deathday = "None"
+            else:
+                deathday = row ["deathday"][:10]
 
+            if row ["picture"] == "":
+                picture = "None"
+            else:
+                picture = row ["picture"]
 
+            if row ["gender"] == "":
+                gender = "None"
+            else:
+                gender = row ["gender"]
 
+            if row ["religionLabel"] == "":
+                religion = "-"
+            else:
+                religion = row ["religionLabel"]
 
+            if row ["predecessor"] == "":
+                predecessor = "None"
+            else:
+                predecessor = row ["predecessor"]
 
+            if row ["successor"] == "":
+                successor = "None"
+            else:
+                successor = row ["successor"]
+
+            line_data = row ["pm20id"] + row ["wikidata_object"] + row ["name"] + row ["birthday"][:10] + deathday + row ["position_held"] + row ["position_held_startdate"][:10] + row ["position_held_enddate"][:10] + picture + row ["country"] + gender + predecessor + successor + religion
+            all_db_entries = Person.objects.all()
+
+            for entry in all_db_entries:
+                db_entry = str(entry.pm20id) + str(entry.wikidata_object) + str(entry.name) + str(entry.birthday) + str(entry.deathday) + str(entry.position_held) + str(entry.position_held_startdate) + str(entry.position_held_enddate) + str(entry.picture) + str(entry.country) + str(entry.gender) + str(entry.predecessor) + str(entry.successor) + str(entry.religion)
+
+                if db_entry == line_data:
+                    new_data = False
+
+            if new_data == True:
+                count_new += 1
+
+                person = Person ()
+                person.pm20id = row ["pm20id"]
+                person.wikidata_object = row ["wikidata_object"]
+                person.name = row ["name"]
+                person.birthday = row ["birthday"] [:10]
+                if row ["deathday"] != "":
+                    person.deathday = row ["deathday"] [:10]
+                person.position_held = row ["position_held"]
+                person.position_held_startdate = row ["position_held_startdate"] [:10]
+                person.position_held_enddate = row ["position_held_enddate"] [:10]
+                if row ["picture"] != "":
+                    person.picture = row ["picture"]
+                person.country = row ["country"]
+                if row ["gender"] != "":
+                    person.gender = row ["gender"]
+                if row ["religionLabel"] != "":
+                    person.religion = row ["religionLabel"]
+                if row ["predecessor"] != "":
+                    person.predecessor = row ["predecessor"]
+                if row ["successor"] != "":
+                    person.successor = row ["successor"]
+                #person.save()
+                
+            else:
+                count_old += 1
+
+    print ("old data: " + str (count_old))
+    print ("new data: " + str (count_new))
