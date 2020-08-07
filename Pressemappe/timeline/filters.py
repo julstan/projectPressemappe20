fc379e02da6ef679445dbf0701284e518f7b8802
Patricia Kaluza <petulacicle@gmail.com>
2020-08-08 01:29:11 +0200

    filters.py angepasst, Zeitraumsuche als DropdownListe ausprobiert, da sonst die Suche nur funktioniert hat, wenn man ein exaktes vorhandenes Datum eingegeben hat

--- Pressemappe/timeline/filters.py
+++ Pressemappe/timeline/filters.py
@@ -7,20 +7,20 @@ from .models import *
 
 class DynamicChoiceMixin(object):    #Auswahlmöglichkeiten werden dynamisch aus dem queryset generiert
 
-    @property #DynamicChoiceFilter von django_filters wird umgeschrieben
+    @property
     def field(self):
         queryset = self.parent.queryset
         field = super(DynamicChoiceMixin, self).field
 
-        choices = list()
+        choices = list() #Die Auswahlmöglichkeiten in der Dropdown-Liste
         have = list()
-        # iteriert durch das queryset und holt die Werte der jeweiligen Feldnamen
+        # iteriert durch das queryset und holt alle Werte der jeweiligen Feldnamen
         for item in queryset:
-            name = getattr(item, self.field_name)
+            name = getattr(item, self.field_name) #Die Feldnamen: gender, religion, country, ...
             if name in have:
                 continue
-            have.append(name)
-            choices.append((name, name))
+            have.append(name) #Hier alle Werte
+            choices.append((name, name)) #name, name = Für die entsprechenden Feldnamen die Werte, die in der Dropdown-Liste zu sehen sind
         field.choices.choices = choices
         return field
 
@@ -38,9 +38,15 @@ class PersonFilter(django_filters.FilterSet):  #Filterset generiert automatisch 
 
 
     #Datum wird extra behandelt, deshalb kommt es unten nicht in fields
-    StartDatum = DateFilter(label='Regierungsbeginn', field_name="position_held_startdate", lookup_expr='gte', widget=TextInput(attrs={'placeholder': 'YYYY-MM-DD', 'class': 'form-control w-100'}))  #größer und gleich als
-    EndDatum = DateFilter(label='Regierungsende', field_name="position_held_enddate", lookup_expr='lte', widget=TextInput(attrs={'placeholder': 'YYYY-MM-DD', 'class': 'form-control w-100'}))  #kleiner und gleich als
+    # Freies Textfeld aber nur exaktes Datum kann gefunden werden
+    # StartDatum = DateFilter(label='Regierungsbeginn', field_name="position_held_startdate", lookup_expr='gte', widget=TextInput(attrs={'placeholder': 'YYYY-MM-DD', 'class': 'form-control w-100'}))  #größer und gleich als
+    # EndDatum = DateFilter(label='Regierungsende', field_name="position_held_enddate", lookup_expr='lte', widget=TextInput(attrs={'placeholder': 'YYYY-MM-DD', 'class': 'form-control w-100'}))  #kleiner und gleich als
 
+    #Dropdown-Liste mit allen Datumsfomaten
+    position_held_startdate = DynamicChoiceFilter(label='Regierungsbeginn', lookup_expr='gte', widget=Select(attrs={'class': 'form-control w-100'}))
+    position_held_enddate = DynamicChoiceFilter(label='Regierungsende', lookup_expr='lte', widget=Select(attrs={'class': 'form-control w-100'}))
+
+    
     gender = DynamicChoiceFilter(label='Geschlecht', widget=Select(attrs={'class': 'form-control w-100'}))
     religion = DynamicChoiceFilter(label='Religion', widget=Select(attrs={'class': 'form-control w-100'}))
     position_held = DynamicChoiceFilter(label='Position', widget=Select(attrs={'class': 'form-control w-100'}))
@@ -48,7 +54,7 @@ class PersonFilter(django_filters.FilterSet):  #Filterset generiert automatisch 
 
     class Meta:
         model = Person #hier steht unsere Model class Person
-        fields = ['name', 'position_held', 'gender', 'country', 'religion'] #Filter die ins Template übergeben werden
+        fields = ['name', 'position_held', 'gender', 'country', 'religion', 'position_held_startdate','position_held_enddate' ] #Filter die ins Template übergeben werden
         exclude = ['pm20id', 'wikidata_object'] #Filter die nicht ins Template übergeben werden sollen
 
 
