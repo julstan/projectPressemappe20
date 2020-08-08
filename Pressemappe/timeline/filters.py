d01e9acc12dfcceecf815b117ca30041d41a5cc8
Patricia Kaluza <petulacicle@gmail.com>
2020-08-08 03:08:40 +0200

    Kommentare hinzugefügt

--- Pressemappe/timeline/filters.py
+++ Pressemappe/timeline/filters.py
@@ -12,17 +12,26 @@ class DynamicChoiceMixin(object):    #Auswahlmöglichkeiten werden dynamisch aus
         queryset = self.parent.queryset
         field = super(DynamicChoiceMixin, self).field
 
-        choices = list() #Die Auswahlmöglichkeiten in der Dropdown-Liste
-        have = list()
-        # iteriert durch das queryset und holt alle Werte der jeweiligen Feldnamen
-        for item in queryset:
-            name = getattr(item, self.field_name) #Die Feldnamen: gender, religion, country, ...
+        choices = [] #Die Auswahlmöglichkeiten in der Dropdown-Liste
+        have = []
+        # iteriert durch die Person class in models und holt alle Werte der jeweiligen Feldnamen
+        for Person in queryset:
+            name = getattr(Person, self.field_name) #Die Feldnamen: gender, religion, country, ...
             if name in have:
-                continue
-            have.append(name) #Hier alle Werte
+                continue #Geht wieder an den Anfang der Schleife
+            have.append(name) #Hier nur neue Werte werden angehängt, damit keine Werte mehrmals vorkommen bsp. nur einmal männlich und weiblich
             choices.append((name, name)) #name, name = Für die entsprechenden Feldnamen die Werte, die in der Dropdown-Liste zu sehen sind
+
+        #wenn die Schleife fertig ist, werden die Auswahlmöglichkeiten für die Dropdown-Liste abgespeichert
         field.choices.choices = choices
         return field
+
+ # if '-' in name and name[:4].isnumeric():
+ #                    have.append(name) #Hier nur neue Werte werden angehängt, damit keine Werte mehrmals vorkommen bsp. nur einmal männlich und weiblich
+ #                    choices.append((name, name[:4])) #name, name = Für die entsprechenden Feldnamen die Werte, die in der Dropdown-Liste zu sehen sind
+ #                else:
+ #                    have.append(name)
+ #                    choices.append((name, name))
 
 
 class DynamicChoiceFilter(DynamicChoiceMixin, django_filters.ChoiceFilter):
@@ -46,7 +55,7 @@ class PersonFilter(django_filters.FilterSet):  #Filterset generiert automatisch 
     position_held_startdate = DynamicChoiceFilter(label='Regierungsbeginn', lookup_expr='gte', widget=Select(attrs={'class': 'form-control w-100'}))
     position_held_enddate = DynamicChoiceFilter(label='Regierungsende', lookup_expr='lte', widget=Select(attrs={'class': 'form-control w-100'}))
 
-    
+
     gender = DynamicChoiceFilter(label='Geschlecht', widget=Select(attrs={'class': 'form-control w-100'}))
     religion = DynamicChoiceFilter(label='Religion', widget=Select(attrs={'class': 'form-control w-100'}))
     position_held = DynamicChoiceFilter(label='Position', widget=Select(attrs={'class': 'form-control w-100'}))
